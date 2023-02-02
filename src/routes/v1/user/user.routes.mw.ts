be0f5b3eac8046c377d4f8304b4a12b28af57e6f
Cristian Ramirez <cristiandramirezm.2024@gmail.com>
2023-02-01 22:54:25 -0500

    User creation an obtantion completed

--- src/routes/v1/user/user.routes.mw.ts
+++ src/routes/v1/user/user.routes.mw.ts
@@ -4,8 +4,25 @@ import { Request, Response, NextFunction } from "express";
 function getUserRoute(req: Request, res: Response, next: NextFunction) {
   try {
     const userData = res.locals.user;
-    res.locals.mssg = "hola mundo";
+
+    res.locals.mssg = "User obtained";
     res.locals.data = userData;
+    res.locals.hasData = true;
+    next();
+  } catch (error) {
+    res.locals.mssg = "User couldnt be obtained";
+
+    next(error);
+  }
+}
+
+//@ts-ignore
+function createUserRoute(req: Request, res: Response, next: NextFunction) {
+  try {
+    const userData = res.locals.user;
+    res.locals.mssg = "User created correctly";
+    res.locals.data = userData;
+    res.locals.hasData = true;
     next();
   } catch (error) {
     res.locals.mssg = "User couldnt be obtained";
@@ -15,6 +32,7 @@ function getUserRoute(req: Request, res: Response, next: NextFunction) {
 
 export default {
   getUserRoute,
+  createUserRoute,
 };
 
-export { getUserRoute };
+export { getUserRoute, createUserRoute };
